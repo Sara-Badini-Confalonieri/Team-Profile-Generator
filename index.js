1189e6a867d1bd616ed5c81c474beb472bdea517
Sara Badini Confalonieri <sara.badiniconfalonieri@gmail.com>
2024-01-24 16:18:47 +0000

    added functions to collect engineer and intern info

--- index.js
+++ index.js
@@ -17,39 +17,115 @@ const teamMembers = [];
 
 // Function to collect manager info
 function createManager() {
-  console.log("Please enter the team manager's information:");
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "Manager's name:",
-      },
-      {
-        type: "input",
-        name: "id",
-        message: "Manager's employee ID Numer:",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "Manager's email:",
-      },
-      {
-        type: "input",
-        name: "officeNumber",
-        message: "Manager's office number:",
-      },
-    ])
-    .then((answers) => {
-      const manager = new Manager(
-        answers.name,
-        answers.id,
-        answers.email,
-        answers.officeNumber
-      );
-      teamMembers.push(manager);
-      createTeam();
-    });
+    console.log("Please enter the team manager's information:");
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Manager's name:",
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "Manager's employee ID Numer:",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Manager's email:",
+            },
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "Manager's office number:",
+            },
+        ])
+        .then((answers) => {
+            const manager = new Manager(
+                answers.name,
+                answers.id,
+                answers.email,
+                answers.officeNumber
+            );
+            teamMembers.push(manager);
+            createTeam();
+        });
 }
 
+// Function to collect engineer info
+function createEngineer() {
+    console.log("Please enter the engineer's information:");
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Engeneer's name:",
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "Engineer's employee ID Number:",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Engineer's email:",
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "Engineer's GitHub username:",
+            },
+        ])
+        .then((answers) => {
+            const engineer = new Engineer(
+                answers.name,
+                answers.id,
+                answers.email,
+                answers.github
+            );
+            teamMembers.push(engineer);
+            createTeam();
+        });
+}
+
+
+// Function to collect intern info
+function createIntern() {
+    console.log("Please enter the intern's information:");
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Intern's name:",
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "Intern's employee ID:",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Intern's email:",
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "Intern's school:",
+            },
+        ])
+        .then((answers) => {
+            const intern = new Intern(
+                answers.name,
+                answers.id,
+                answers.email,
+                answers.school
+            );
+            teamMembers.push(intern);
+            createTeam();
+        });
+}
